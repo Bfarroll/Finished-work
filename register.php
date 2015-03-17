5aa1a1339043a24db596fcc5bda6a7c926f39165
Billy Farroll <watanight@hotmail.co.uk>
2015-03-17 13:44:32 +0000

    Create register.php

---
+++ register.php
@@ -0,0 +1,30 @@
+<?php require_once("session.php"); ?>
+<?php require_once("connect.php"); ?>
+<?php require_once("functions.php"); ?>
+<?php include_once("header.php"); ?>
+
+
+<?php    
+    if(isset($_POST["submit"])) {
+        $username = ($_POST["username"]);
+        $password = ($_POST["password"]);
+        $city = ($_POST["city"]);
+        $query = "INSERT INTO users (username, password, city) VALUES ('{$username}','{$password}','{$city}')";
+        $result = mysqli_query($connection, $query);
+        
+        if($result) {
+            $message = "Post added";
+        } else {
+            $message = "Something went wrong";
+        }
+    } 
+?>
+
+<div class="register">
+            <form action="index.php" method="post">
+                <p>Username:</p><input type="text" name="username" value=""/><br>
+                <p>Password:</p><input type="text" name="password" value=""/><br>
+                <p>City:</p><input type="text" name="city" value=""/><br><br>
+                <input type="submit" name="submit" value="Register"/>
+            </form>
+        </div>
