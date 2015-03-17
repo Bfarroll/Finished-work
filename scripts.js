ef39756fdb4b1b992143fcd3a11024f6f1e93c99
Billy Farroll <watanight@hotmail.co.uk>
2015-03-17 13:44:55 +0000

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    
+   $('.login-box').hide();
+    
+    $('.login-toggle').click(function() {
+        $('.login-box').slideToggle();
+    });
+    
+    $('.register').hide();
+    
+    $('.register-toggle').click(function() {
+        $('.register').slideToggle();
+    });
+    
+    $('.add-holiday').hide();
+    
+    $('.holiday-toggle').click(function() {
+        $('.add-holiday').slideToggle();
+    });
+    
+});
